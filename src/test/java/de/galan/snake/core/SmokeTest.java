c89be9d97d54070d0830d882eba8a2fe2ce3a749
Daniel Galán y Martins <git@galan.de>
2015-05-29 17:27:58 +0200

    package

--- src/test/java/de/galan/snake/core/SmokeTest.java
+++ src/test/java/de/galan/snake/core/SmokeTest.java
@@ -3,7 +3,7 @@ package de.galan.snake.core;
 import org.junit.Test;
 
 import de.galan.commons.time.Sleeper;
-import de.galan.snake.client.BootstrapSnake;
+import de.galan.snake.boot.BootstrapSnake;
 import de.galan.snake.client.Snake;
 
 
